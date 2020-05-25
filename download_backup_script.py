1a8f3196210440fbe800f325158833b9c2be4620
Tobias Dybdahl <tdybda17@student.aau.dk>
2020-05-25 14:22:40 +0200

    date changes

--- download_backup_script.py
+++ download_backup_script.py
@@ -5,15 +5,17 @@ import requests
 
 url = 'http://127.0.0.1:8000/backup/'
 credentials_path = 'credentials.json'
-backup_output_path = 'somefile.tar.gz"'
+backup_output_path = 'somefile.tar.gz'
+date = '2020-04-04'
 
 with open(credentials_path) as json_file:
     config = json.load(json_file)
+
 r = requests.get(
     url=url,
     verify=False,
     stream=True,
-    headers={'USER': config.get('USERNAME'), 'PASS': config.get('PASSWORD')}
+    headers={'USER': config.get('USERNAME'), 'PASS': config.get('PASSWORD'), 'Date': date},
 )
 r.raw.decode_content = True
 with open(backup_output_path, 'wb') as f:
